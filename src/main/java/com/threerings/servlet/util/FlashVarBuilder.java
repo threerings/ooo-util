0b6ad6731182858d2d7a5d11688b10fe20c624ae
Michael Bayne <mdb@samskivert.com>
2011-10-05 10:07:52 -0700

    Reheadered.

--- src/main/java/com/threerings/servlet/util/FlashVarBuilder.java
+++ src/main/java/com/threerings/servlet/util/FlashVarBuilder.java
@@ -1,5 +1,7 @@
 //
-// $Id$
+// ooo-util - a place for OOO utilities
+// Copyright (C) 2011 Three Rings Design, Inc., All Rights Reserved
+// http://github.com/threerings/ooo-util/blob/master/LICENSE
 
 package com.threerings.servlet.util;
 
